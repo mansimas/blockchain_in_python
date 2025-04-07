dd5753fd1fbb83177eb0b3ed32f1c24df7cca64c
mantas <magimantas@yahoo.com>
2025-04-07 20:15:15 +0300

    integrated genesis into default blockchain

--- blockchain.py
+++ blockchain.py
@@ -7,10 +7,10 @@ class Blockchain:
   """
 
   def __init__(self):
-    self.chain = []
+    self.chain = [genesis()]
 
   def add_block(self, data):
-    self.chain.append(Block(data))
+    self.chain.append(mine_block(self.chain[-1], data))
 
   def __repr__(self):
     return f'Blockchain({self.chain})'
