d4dd945dc92b24507b7a1d72e78b4187ae1e9dd4
mantas <magimantas@yahoo.com>
2025-04-07 20:18:54 +0300

    moved genesis and mine_block into Block class

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-from block import Block, genesis, mine_block
+from block import Block
 
 class Blockchain:
   """
@@ -7,19 +7,19 @@ class Blockchain:
   """
 
   def __init__(self):
-    self.chain = [genesis()]
+    self.chain = [Block.genesis()]
 
   def add_block(self, data):
-    self.chain.append(mine_block(self.chain[-1], data))
+    self.chain.append(Block.mine_block(self.chain[-1], data))
 
   def __repr__(self):
     return f'Blockchain({self.chain})'
 
 def main():
   blockchain = Blockchain()
-  # blockchain.add_block("Transaction 1")
-  # blockchain.add_block("Transaction 2")
-  # blockchain.add_block("Transaction 3")
+  blockchain.add_block("Transaction 1")
+  blockchain.add_block("Transaction 2")
+  blockchain.add_block("Transaction 3")
   print(blockchain)
 
 if __name__ == '__main__':
