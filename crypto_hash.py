ccc27e197ed29f5c9e7d04c7e16b4a1170c3d4d9
mantas <magimantas@yahoo.com>
2025-04-07 20:43:13 +0300

    finished and integrated crypto hashing

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,21 +1,17 @@
 import hashlib
 import json
 
-def stringify(data):
-    return json.dumps(data)
-
 def crypto_hash(*args):
     """
     Return a sha-256 hash of the given arguments.
     """
-    stringified_args = map(stringify, args)
-    print(f'stringified_args: {stringified_args}')
+    stringified_args =sorted(map(lambda data: json.dumps(data), args))
     joined_data = ''.join(stringified_args)
-    print(f'joined_data: {joined_data}')
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
     print(f"crypto_hash(): {crypto_hash('one', 2, [3])}")
+    print(f"crypto_hash(): {crypto_hash(2, 'one', [3])}")
 
 if __name__ == '__main__':
     main()
