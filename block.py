9e7986a2673e6594067a4ff4c7607fa727311ac0
mantas <magimantas@yahoo.com>
2025-04-07 20:13:19 +0300

    initial commit

---
+++ block.py
@@ -0,0 +1,39 @@
+import time
+
+def mine_block(last_block, data):
+  """
+  Mine a block based on the given last_block and data.
+  """
+  timestamp = time.time_ns()
+  last_hash = last_block.hash
+  hash = f'{timestamp}-{last_hash}'
+
+  return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+  """
+  Generate the genesis block.
+  """
+  return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+class Block:
+  """
+  Block: a unit of storage.
+  Store transactions in a blockchain that supports a cryptocurrency.
+  """
+  def __init__(self, timestamp, last_hash, hash, data):
+    self.timestamp = timestamp
+    self.last_hash = last_hash
+    self.hash = hash
+    self.data = data
+
+  def __repr__(self):
+    return (f'Block(timestamp: {self.timestamp}, last_hash: {self.last_hash}, hash: {self.hash}, data: {self.data})')
+
+def main():
+  genesis_block = genesis()
+  block = mine_block(genesis_block, 'foo')
+  print(block)
+
+if __name__ == '__main__':
+  main()
---
+++ blockchain.py
@@ -0,0 +1,26 @@
+from block import Block, genesis, mine_block
+
+class Blockchain:
+  """
+  Blockchain: a public ledger of transactions.
+  Implemented as a list of blocks - data sets of transactions.
+  """
+
+  def __init__(self):
+    self.chain = []
+
+  def add_block(self, data):
+    self.chain.append(Block(data))
+
+  def __repr__(self):
+    return f'Blockchain({self.chain})'
+
+def main():
+  blockchain = Blockchain()
+  # blockchain.add_block("Transaction 1")
+  # blockchain.add_block("Transaction 2")
+  # blockchain.add_block("Transaction 3")
+  print(blockchain)
+
+if __name__ == '__main__':
+  main()
