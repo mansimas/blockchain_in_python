e47b88b60fceccbe005c30620648a56d96266abd
mantas <magimantas@yahoo.com>
2025-04-07 20:38:12 +0300

    improved sha256 to accept more complex data structures

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,15 +1,21 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def stringify(data):
+    return json.dumps(data)
+
+def crypto_hash(*args):
     """
-    Return a sha-256 hash of the given data.
+    Return a sha-256 hash of the given arguments.
     """
-    stringified_data = json.dumps(data)
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    stringified_args = map(stringify, args)
+    print(f'stringified_args: {stringified_args}')
+    joined_data = ''.join(stringified_args)
+    print(f'joined_data: {joined_data}')
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+    print(f"crypto_hash(): {crypto_hash('one', 2, [3])}")
 
 if __name__ == '__main__':
     main()
