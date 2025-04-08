0745deca51f4715c97b54665ecbd0c847c4615c2
mantas <magimantas@yahoo.com>
2025-04-08 16:07:40 +0300

    added block validation tests

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -87,7 +87,7 @@ class Block:
       - the block must be a valid combination of the block's properties
       """
       if block.last_hash != last_block.hash:
-          raise Exception('The block last_hash must be correct')
+          raise Exception('last_hash must be correct')
 
       if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
           raise Exception('The proof of work requirement was not met')
