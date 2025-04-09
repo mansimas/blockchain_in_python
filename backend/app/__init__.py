1657c52dc55e94588b75e5148e382fd9e6f1e508
mantas <magimantas@yahoo.com>
2025-04-09 23:05:46 +0300

    added a peer instance for testing multiple applications

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
+import os, random
 from backend.blockchain.blockchain import Blockchain
+from backend.pubsub import PubSub
 
 app = Flask(__name__)
 blockchain = Blockchain()
+pubsub = PubSub()
 
 @app.route('/')
 def route_default():
@@ -18,4 +21,9 @@ def route_blockchain_mine():
     blockchain.add_block(transaction_data)
     return jsonify(blockchain.chain[-1].to_json())
 
-app.run()
+PORT = 5000
+
+if os.environ.get('PEER') == 'True':
+    PORT = random.randint(5001, 6000)
+
+app.run(port=PORT)
