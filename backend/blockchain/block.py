6dc2579d79539f0b453c07a7b865db52bee88d50
mantas <magimantas@yahoo.com>
2025-04-09 23:37:54 +0300

    avoiding redundand pubsub interactions

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -72,6 +72,13 @@ class Block:
     return Block(**GENESIS_DATA)
   
   @staticmethod
+  def from_json(block_json):
+    """
+    Deserialize a block from JSON.
+    """
+    return Block(**block_json)
+  
+  @staticmethod
   def adjust_difficulty(last_block, new_timestamp):
     """
     Calculate the adjusted difficulty to the MINE_RATE.
--- backend/pubsub.py
+++ backend/pubsub.py
@@ -2,16 +2,27 @@ from dotenv import load_dotenv
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
+from backend.blockchain.block import Block
 import os, time, uuid
 
 load_dotenv()
 
 class Listener(SubscribeCallback):
-    def presence(self, pubnub, presence):
-        print(f'\n-- Presence Event: {presence.event}')
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
 
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
+        if message_object.channel == CHANNELS['BLOCK']:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print(f'\n -- Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = os.getenv('PUBNUB_SUBSCRIBE_KEY')
@@ -28,10 +39,10 @@ class PubSub():
     Handles the publish/subscribe layer of the application.
     Provides communication between the nodes of the blockchain network.
     """
-    def __init__(self):
+    def __init__(self, blockchain):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener())
+        self.pubnub.add_listener(Listener(blockchain))
 
     def publish(self, channel, message):
         """
