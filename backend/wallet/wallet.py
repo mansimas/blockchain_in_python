e32fcf3942ea38bcd704cc467bd40008426999f0
mantas <magimantas@yahoo.com>
2025-04-10 11:55:32 +0300

    added wallet verification method

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -21,6 +21,17 @@ class Wallet:
     Generate a signature based on the data using the local private key.
     """
     return self.private_key.sign(json.dumps(data).encode('utf8'), ec.ECDSA(hashes.SHA256()))
+  
+  @staticmethod
+  def verify(public_key, data, signature):
+    """
+    Verify a signature using the public key.
+    """
+    try:
+      public_key.verify(signature, json.dumps(data).encode('utf8'), ec.ECDSA(hashes.SHA256()))
+      return True
+    except Exception:
+      return False
 
 def main():
   wallet = Wallet()
@@ -28,6 +39,12 @@ def main():
   data = { "foo": "bar" }
   signature = wallet.sign(data)
   print(f"Signature: {signature}")
+
+  should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+  print(f"Signature valid: {should_be_valid}")
+
+  should_be_invalid = Wallet.verify(Wallet().public_key, data, signature)
+  print(f"Signature valid: {should_be_invalid}")
 
 if __name__ == "__main__":
   main()
