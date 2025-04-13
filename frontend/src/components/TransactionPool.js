797d8b35d077d758d6954f5786f38b43cbfafae7
mantas <magimantas@yahoo.com>
2025-04-13 17:56:16 +0300

    added transactions pool frontend data

---
+++ frontend/src/components/TransactionPool.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Transaction from "./Transaction";
+import { API_BASE_URL } from "../config";
+
+function TransactionPool() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/transactions`)
+      .then((r) => r.json())
+      .then((json) => {
+        setTransactions(json);
+      });
+  }, []);
+
+  return (
+    <div className="TransactionPool">
+      <Link to="/">Home</Link>
+      <hr />
+      <h3>Transaction Pool</h3>
+      <div>
+        {transactions.map((transaction) => (
+          <div key={transaction.id}>
+            <hr />
+            <Transaction transaction={transaction} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TransactionPool;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 import App from "./components/App";
 import Blockchain from "./components/Blockchain";
 import ConductTransaction from "./components/ConductTransaction";
+import TransactionPool from "./components/TransactionPool";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +16,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/blockchain" element={<Blockchain />} />
         <Route path="/conduct-transaction" element={<ConductTransaction />} />
+        <Route path="/transaction-pool" element={<TransactionPool />} />
       </Routes>
     </Router>
   </React.StrictMode>
