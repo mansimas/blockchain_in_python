f5f17f6c8d45f5cfbe44914ca221e373640d98f9
mantas <magimantas@yahoo.com>
2025-04-13 18:06:12 +0300

    added transactions polling

--- frontend/src/components/TransactionPool.js
+++ frontend/src/components/TransactionPool.js
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Transaction from "./Transaction";
-import { API_BASE_URL } from "../config";
+import { API_BASE_URL, SECONDS_JS } from "../config";
+
+const POLL_INTERVAL = 10 * SECONDS_JS;
 
 function TransactionPool() {
   const [transactions, setTransactions] = useState([]);
-
-  useEffect(() => {
+  const fetchTransactions = () => {
     fetch(`${API_BASE_URL}/transactions`)
       .then((r) => r.json())
       .then((json) => {
         setTransactions(json);
       });
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+    setInterval(fetchTransactions, POLL_INTERVAL);
   }, []);
 
   return (
