e6be11a0b25f69acf2a4dc3161779263d1fa1ede
mantas <magimantas@yahoo.com>
2025-04-13 14:54:25 +0300

    added hide, show for Blocks

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { MILLISECONDS_PY } from "../config";
 import Transaction from "./Transaction";
+
+function ToggleTransactionDisplay({ block }) {
+  const [displayTransaction, setDisplayTransaction] = useState(false);
+  const { data } = block;
+  const toggleDisplayTransaction = () => {
+    setDisplayTransaction(!displayTransaction);
+  };
+
+  if (displayTransaction) {
+    return (
+      <div>
+        {data.map((transaction) => (
+          <div key={transaction.id}>
+            <hr />
+            <Transaction transaction={transaction} />
+          </div>
+        ))}
+        <br />
+        <Button variant="danger" size="sm" onClick={toggleDisplayTransaction}>
+          Show Less
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <br />
+      <Button variant="danger" size="sm" onClick={toggleDisplayTransaction}>
+        Show More
+      </Button>
+    </div>
+  );
+}
 
 function Block({ block }) {
   const { timestamp, hash, data } = block;
@@ -12,14 +48,7 @@ function Block({ block }) {
     <div className="Block">
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {timestampDisplay}</div>
-      <div>
-        {data.map((transaction) => (
-          <div key={transaction.id}>
-            <hr />
-            <Transaction transaction={transaction} />
-          </div>
-        ))}
-      </div>
+      <ToggleTransactionDisplay block={block} />
     </div>
   );
 }
