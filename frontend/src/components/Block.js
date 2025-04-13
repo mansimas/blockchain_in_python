8a426e769604edf0978c8c39cec102098b371dba
mantas <magimantas@yahoo.com>
2025-04-13 14:24:58 +0300

    added Transaction component

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,4 +1,5 @@
 import { MILLISECONDS_PY } from "../config";
+import Transaction from "./Transaction";
 
 function Block({ block }) {
   const { timestamp, hash, data } = block;
@@ -11,7 +12,14 @@ function Block({ block }) {
     <div className="Block">
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {timestampDisplay}</div>
-      <div>{JSON.stringify(data)}</div>
+      <div>
+        {data.map((transaction) => (
+          <div key={transaction.id}>
+            <hr />
+            <Transaction transaction={transaction} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
